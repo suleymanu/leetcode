80b9fd964ca71782a1f2bd2e35f0747798ae80ba
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-07-06 15:10:26 -0400

    Create Solution.java

---
+++ LongestCommonPrefix/Solution.java
@@ -0,0 +1,13 @@
+/* O(n) time. O(1) space. [n: number of total characters] Easy. */
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        for(int i=0; i<strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for(int j=1; j<strs.length; j++) {
+                if(strs[j].length() == i || strs[j].charAt(i) != c)
+                    return strs[0].substring(0,i);
+            }
+        }
+        return strs[0];
+    }
+}
