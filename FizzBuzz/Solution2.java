4d8037fe05918796f091b1e19be3998654cf876b
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-07-06 17:56:55 -0400

    Create Solution2.java

---
+++ FizzBuzz/Solution2.java
@@ -0,0 +1,45 @@
+/* Could be a fast alternative. Creats groups of 15. */
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> output = new ArrayList<>(n);
+        int i=1;
+        while(i<=n/15*15) {
+            output.add(String.valueOf(i));
+            output.add(String.valueOf((i+1)));
+            output.add("Fizz");
+            output.add(String.valueOf((i+3)));
+            output.add("Buzz");
+            output.add("Fizz");
+            output.add(String.valueOf((i+6)));
+            output.add(String.valueOf((i+7)));
+            output.add("Fizz");
+            output.add("Buzz");
+            output.add(String.valueOf((i+10)));
+            output.add("Fizz");
+            output.add(String.valueOf((i+12)));
+            output.add(String.valueOf((i+13)));
+            output.add("FizzBuzz");
+            i+=15;
+        }
+        while(i<=n) {
+            int remainder = i%15;
+            switch (remainder) {
+                case 5:
+                case 10:
+                    output.add("Buzz");
+                    break;
+                case 3:
+                case 6:
+                case 9:
+                case 12:
+                    output.add("Fizz");
+                    break;
+                default:
+                    output.add(String.valueOf(i));
+                    break;
+            }
+            i++;
+        }
+        return output;
+    }
+}
