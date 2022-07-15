fb9bb20c79b42d233db25121625105c8ca9287ff
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-07-15 14:29:41 -0400

    Create Solution.java

---
+++ FirstUniqueCharacterInAString/Solution.java
@@ -0,0 +1,12 @@
+/* O(n) time. O(n) space. Two pass. One array to count occurance. */
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] count = new int[26];
+        for(char c : s.toCharArray())
+            count[c-'a']++;
+        for(int i=0; i<s.length(); i++)
+            if(count[s.charAt(i)-'a']==1)
+                return i;
+        return -1;
+    }
+}
