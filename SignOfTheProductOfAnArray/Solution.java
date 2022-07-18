24b4088556ae1cbd0e26f29da146144963dc7bb4
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-07-18 15:13:27 -0400

    Create Solution.java

---
+++ SignOfTheProductOfAnArray/Solution.java
@@ -0,0 +1,11 @@
+/* O(n) time. O(1) space. */
+class Solution {
+    public int arraySign(int[] nums) {
+        int sign=1;
+        for(int n : nums) {
+            if(n==0) return 0;
+            else if(n<0) sign=-sign;
+        }
+        return sign;
+    }
+}
