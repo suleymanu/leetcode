9a81320f201195fd3926159d3ada5b7d974bfa51
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-07-25 20:32:55 -0400

    Create Solution2.java

---
+++ PowerOfTwo/Solution2.java
@@ -0,0 +1,11 @@
+/* Division. */
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        while(n>1) {
+            if(n%2==1) return false;
+            n /= 2;
+        }
+        return true;
+    }
+}
