a79cf79212d0eaf79d2163e244ca1e60535cf31e
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-07-27 14:31:09 -0400

    Create Solution3.java

---
+++ NumberOf1Bits/Solution3.java
@@ -0,0 +1,12 @@
+/* Smart bit manipulation. */
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int sum=0;
+        while(n!=0) {
+            sum++;
+            n &= (n-1);
+        }
+        return sum;
+    }
+}
