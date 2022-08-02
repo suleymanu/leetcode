42a39f1d1a47f77963008adc5895a12467852ed9
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-08-02 16:21:38 -0400

    Create Solution2.java

---
+++ RunningSumOf1DArray/Solution2.java
@@ -0,0 +1,8 @@
+/* In-place sum. */
+class Solution2 {
+    public int[] runningSum(int[] nums) {
+        for(int i=1; i<nums.length; i++)
+            nums[i] = nums[i-1]+nums[i];
+        return nums;
+    }
+}
