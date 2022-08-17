c228c70749c7509de90ad128dc7d966b5a173faa
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-08-17 12:25:57 -0400

    Create Solution.java

---
+++ ImplementStackUsingQueues/Solution.java
@@ -0,0 +1,39 @@
+/* One queue. Reverses elements at each push. */
+class MyStack {
+    
+    Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList();
+    }
+    
+    public void push(int x) {
+        q.offer(x);
+        int size = q.size();
+        while(size > 1) {
+            q.offer(q.poll());
+            --size;
+        }
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
