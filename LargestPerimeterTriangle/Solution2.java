9f2db71845d8e77dc927bc76bf6ad8d2237fe5a1
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-08-24 15:07:28 -0400

    Create Solution2.java

---
+++ LargestPerimeterTriangle/Solution2.java
@@ -0,0 +1,18 @@
+/* First trial. No need to sort descending. */
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        int r = 0;
+        Arrays.sort(nums);
+        for(int i=0; i<(nums.length/2); i++) {
+            int t = nums[nums.length-1-i];
+            nums[nums.length-1-i] = nums[i];
+            nums[i] = t;
+        }
+        for(int i=0; i<nums.length-2; i++)
+            if(nums[i] < (nums[i+1]+nums[i+2])) {
+                r = nums[i]+nums[i+1]+nums[i+2];
+                break;
+            }
+        return r;
+    }
+}
