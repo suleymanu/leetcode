9578411c3caa280c84fbbfa4fc8b9286672a79fb
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-08-30 14:47:45 -0400

    Create Solution2.java

---
+++ IsSubsequence/Solution2.java
@@ -0,0 +1,11 @@
+/* Shorter version. */
+class Solution2 {
+    public boolean isSubsequence(String s, String t) {
+        int i=0, j=0;
+        while(i<s.length() && j<t.length()) {
+            if(s.charAt(i)==t.charAt(j)) i++;
+            j++;
+        }
+        return i==s.length();
+    }
+}
