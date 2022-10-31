ad569e95d595fbe8ba16cb441044e8b29c1f442d
Suleyman Uslu <suleymanu@users.noreply.github.com>
2022-10-31 19:46:16 -0400

    Create Solution.java

---
+++ TwoSumIVInputIsABST/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    Set<Integer> set = new HashSet();
+    public boolean findTarget(TreeNode root, int k) {
+        if(root!=null) {
+            if(set.contains(k-root.val)) return true;
+            else set.add(root.val);
+            return findTarget(root.left,k) || findTarget(root.right,k);
+        } else return false;
+    }
+}
