0654d10889704992752046f22d1f4c5cd933e9f8
Suleyman Uslu <suleymanu@users.noreply.github.com>
2023-01-27 22:31:28 -0500

    Create Solution2.java

---
+++ MinimumAbsoluteDifference/Solution2.java
@@ -0,0 +1,32 @@
+/* Counting sort */
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+        for(int a:arr) {
+            min = Math.min(min,a);
+            max = Math.max(max,a);
+        }
+        int len = max-min+1;
+        int[] s = new int[len];
+        int sf = min;
+        for(int a:arr) s[a-sf] = 1;
+        int p = min;
+        int minD = 2000001;
+        for(int i=1; i<len; i++) {
+            if(s[i]==1) {
+                if(i+sf-p<minD) minD = i+sf-p;
+                p = i+sf;
+            }
+        }
+        List<List<Integer>> r = new ArrayList();
+        p = min;
+        for(int i=1; i<len; i++) {
+            if(s[i]==1) {
+                if(i+sf-p==minD) r.add(Arrays.asList(p,i+sf));
+                p = i+sf;
+            }
+        }
+        return r;
+    }
+}
