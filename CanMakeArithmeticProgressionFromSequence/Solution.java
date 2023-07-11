d2fbc8080b8a107ef55ad6c636f33542150d4b2e
Suleyman Uslu <suleymanu@users.noreply.github.com>
2023-07-11 12:39:14 -0400

    Create Solution.java

---
+++ CanMakeArithmeticProgressionFromSequence/Solution.java
@@ -0,0 +1,10 @@
+/* O(nlogn) time due to sorting. No additional space. */
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int d = arr[1]-arr[0];
+        for(int i=2; i<arr.length; i++)
+            if(arr[i]-arr[i-1] != d) return false;
+        return true;
+    }
+}
