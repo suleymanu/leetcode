172d0e0e5a386dfec1e1065f9ba2cabd10343e95
Suleyman Uslu <suleymanu@users.noreply.github.com>
2023-08-23 14:14:45 -0400

    Create Solution2.java

---
+++ PerfectNumber/Solution2.java
@@ -0,0 +1,6 @@
+/* Not many perfects till 10^8. */
+class Solution2 {
+    public boolean checkPerfectNumber(int num) {
+        return num==6||num==28||num==496||num==8128||num==33550336;
+    }
+}
