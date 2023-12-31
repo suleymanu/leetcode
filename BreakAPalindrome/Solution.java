f146e5e0ae9b6a2d1dfcadd6089d837c317579b7
Suleyman Uslu <suleymanu@users.noreply.github.com>
2023-12-31 18:43:03 -0500

    Create Solution.java

---
+++ BreakAPalindrome/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        if(palindrome.length()<2) return "";
+        char[] a = palindrome.toCharArray();
+        for(int i=0; i<a.length/2; ++i)
+            if(a[i]>'a') {a[i]='a'; return new String(a);}
+        a[a.length-1]='b';
+        return new String(a);
+    }
+}
